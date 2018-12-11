20bbacc431728ae2fb2758105eeaea990e0bbc6c
HardenV <44287727+HardenV@users.noreply.github.com>
2018-12-11 08:40:51 +0800

    Update TCPKernel.h

--- TCPKernel.h
+++ TCPKernel.h
@@ -1,119 +1,32 @@
 #pragma once
-
-//#ifndef _TCPKERNEL_H
-
-//#define _TCPKERNEL_H
-
-#include "CMySql.h"
-
-#include "TCPNet.h"
-
-#include "thread_pool.h"
-
-#include "Packdef.h"
-
-#include "Online.h"
-
+#include"Mysql.h"
+#include"TCPNet.h"
+#include"threadpool.h"
+#include"Packdef.h"
 class TCPKernel;
-
 class TCPNet;
-
 class CMySql;
-
-//class threadpool;
-
-typedef void (TCPKernel:: *PFUN)(int ,char*);
-
+typedef void (TCPKernel::*PFUN)(int ,char*);
 struct ProtocolMap
-
 {
-
-	    PackType m_nType;
-
-		PFUN     m_pfun;
-
+	PackType m_nType;
+	PFUN     m_pfun;
 };
-
-
-
-
 
 class TCPKernel
-
 {
-
 public:
-
 	TCPKernel();
-
-   virtual ~TCPKernel();
-
+	virtual ~TCPKernel();
 public:
-
-	 bool Open() ;
-
-	 void Close() ;
-
-	 void DealData(int sockfd,char* szbuf) ;
-
+	bool Open();
+	void Close();
+	void DealData(int sockfd,char* szbuf) ;
 public:
-
 	void RegisterRq(int sockfd,char* szbuf);
-
 	void LoginRq(int sockfd,char* szbuf);
-
-public:
-
-	TCPNet *m_pTcpNet;
-
+	void PublishRq(int sockfd,char* szbuf);
 private:
-
+	TCPNet* m_pTcpNet;
 	CMySql m_sql;
-
-	list<OlAccount*> m_lstOlAccount;
-
-//	threadpool m_threadpool;
-
-
-
-
-
 };
-
-
-
-/*class RedItask
-
-{
-
-public:
-
-	    RedItask(int sockfd,TCPKernel*pKernel)
-
-		{
-
-			m_sockfd = sockfd;
-
-	
-
-			m_pKernel = pKernel;
-
-		}
-
-		~RedItask(){}
-
-		void RunItask();
-
-private:
-
-										
-
-		int  m_sockfd;											
-
-	    TCPKernel* m_pKernel;
-
-
-
-};*/
-
-//#endif
