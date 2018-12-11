6a2aa95ec645646f2165830af74a03c846b6e668
HardenV <44287727+HardenV@users.noreply.github.com>
2018-12-11 08:43:12 +0800

    Update TCPNet.cpp

--- TCPNet.cpp
+++ TCPNet.cpp
@@ -1,66 +1,102 @@
-#include "TCPNet.h"
+#include"TCPNet.h"
 
 TCPNet::TCPNet(TCPKernel* tcpKernel)
 {
-//	printf("TCPNet init\n");
-	m_pTCPKernel = tcpKernel;	
-    is_stop=false;
+	m_pTCPKernel=tcpKernel;
+	is_stop=false;
 	port=8000;
 	pool=NULL;
-	threadnum=20;
+	threadnum=2;
 }
-
-bool TCPNet::InitNetWork()   //EpollServer的初始化
+bool TCPNet::InitNetWork()
 {
-	bzero(&bindAddr, sizeof(bindAddr));
-	bindAddr.sin_family = AF_INET;
-	bindAddr.sin_port = htons(port);
-	bindAddr.sin_addr.s_addr = inet_addr("10.50.66.240");  //创建Socket
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd < 0)
+	bzero(&bindAddr,sizeof(bindAddr));
+	bindAddr.sin_family=AF_INET;
+	bindAddr.sin_port=htons(port);
+	bindAddr.sin_addr.s_addr=inet_addr(SERVER_IP);
+	sockfd=socket(AF_INET,SOCK_STREAM,0);
+	if(sockfd<0)
 	{
-		printf("EpollServer socket init error\n");
+		printf("server initerror...\n");
 		return false;
 	}
-	
-	//绑定
-	int ret = bind(sockfd, (struct sockaddr *)&bindAddr, sizeof(bindAddr));
-	if(ret < 0)
+	int ret=bind(sockfd,(struct sockaddr*)&bindAddr,sizeof(bindAddr));
+	if(ret<0)
 	{
-		printf("EpollServer bind init error\n");
+		printf("server bind error...\n");
 		return false;
-	}
-	
-	//监听
-	ret = listen(sockfd, 10);
-	if(ret < 0)
-	{
-		printf("EpollServer listen init error\n");
-		return false;
-	}
-	
-    //创建Epoll
-	epollfd = epoll_create(1024);
-	if(epollfd < 0)
-	{
-		printf("EpollServer epoll_create init error\n");
-		return false;
-	}
-	pool = new threadpool<BaseTask>(threadnum);  //创建线程池
 
-	//将sockfd加入epoll
+	}
+	ret =listen(sockfd,10);
+	if(ret<0)
+	{
+		printf("server listen error..\n");
+		return false;
+	}
+	epollfd=epoll_create(1024);
+	if(epollfd<0)
+	{
+		printf("server create epoll error...\n");
+		return false;
+	}
+	pool =new threadpool<BaseTask>(threadnum);//创建线程池
 	return true;
 }
 
 void Task::doit()
-{
-		//do something of the order
-		//printf("%s\n", order);
-//		snprintf(order, MAX_BUFFER - 1, "somedata\n");
-	        //write(sockfd, order, strlen(order));
-        m_TCPKernel->DealData(sockfd,order);
+{                    
+	char buffer[MAX_BUFFER];
+    memset(buffer, 0, sizeof(buffer));
+       
+    while(1)
+    {
+		//先按包头大小收包头 获取实际数据长度
+		int ret = recv(sockfd,buffer, sizeof(STRU_HEADER),0);
+		int pac_size;
+		if(ret > 0)
+		{
+			STRU_HEADER* shead = (STRU_HEADER*)buffer;
+			pac_size = shead->pac_size;
+			printf("%d\n",pac_size);
+			//再按实际长度收取数据
+			ret = recv(sockfd, buffer, pac_size ,0);
+			printf("ret %d\n",ret);
+			//printf("%s\n",buffer);
+			//某个fd关闭了连接，从Epoll中删除并关闭fd
+			if(ret == 0)
+			{
+				struct epoll_event ev;
+				ev.events = EPOLLIN;
+				ev.data.fd = sockfd;
+				epoll_ctl(repollfd, EPOLL_CTL_DEL, sockfd, &ev);
+				shutdown(sockfd, SHUT_RDWR);
+				printf("%d 接收完成\n", sockfd);
+				break;
+			}
+			//读取出错，尝试再次读取
+			else if(ret < 0)
+			{
+				if(errno == EAGAIN)	
+				{
+					printf("read error! read again\n");
+                    reset_oneshot(repollfd,sockfd);
+                    cout<<"reset epollfd"<<endl;
+					break;
+				}
+			}
+			//成功读取，将读取信息发送给中介者
+			else
+			{
+				m_TCPKernel->DealData(sockfd,buffer);
+			}
+   		}
+		else if(ret < 0)
+		{
+			cout<<"error"<<endl;
+			break;
+		}
+	} 
 }
-
 void TCPNet::DealData()
 {
 	pool->start();
@@ -88,41 +124,24 @@ void TCPNet::DealData()
 					&clientAddr, &len);
  
 				//将新的confd加入epoll中
-				TCPNet::addfd(epollfd, confd, false);
+				//TCPNet::addfd(epollfd, confd, true);
+				int recvsize = 500*1024*1024;
+				if(setsockopt(fd, SOL_SOCKET,SO_RCVBUF,(const char*)&recvsize,sizeof(int)) == -1)
+					printf("setsocket error\n");
+				else
+					printf("setsocket success\n");
+				TCPNet::addfd(epollfd, confd, true);
 			}
 			else if(events[i].events & EPOLLIN)  //某个fd上有数据可读
 			{
-				char buffer[MAX_BUFFER];
-		readagain:	memset(buffer, 0, sizeof(buffer));
-				int ret = recv(fd, buffer, MAX_BUFFER - 1,0);
-				//某个fd关闭了连接，从Epoll中删除并关闭fd
-				if(ret == 0)
-				{
-					struct epoll_event ev;
-					ev.events = EPOLLIN;
-					ev.data.fd = fd;
-					epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, &ev);
-					shutdown(fd, SHUT_RDWR);
-					printf("%d logout\n", fd);
-					continue;
-				}
-				//读取出错，尝试再次读取
-				else if(ret < 0)
-				{
-					if(errno == EAGAIN)	
-					{
-						printf("read error! read again\n");
-						goto readagain;
-					    	break;
-					}
-				}
-				//成功读取，将读取信息发送给中介者
+				//更改sock缓冲区大小
+				/*int recvsize = 500*1024*1024;
+				if(setsockopt(fd, SOL_SOCKET,SO_RCVBUF,(const char*)&recvsize,sizeof(int)) == -1)
+					printf("setsocket error\n");
 				else
-				{ //printf("aaa\n");
-					Task *task = new Task(buffer,fd,m_pTCPKernel);
-					pool->append_task(task);
-			//		m_pTCPKernel->DealData(fd,buffer);
-				}
+					printf("setsocket success\n");*/
+                BaseTask *task = new Task(epollfd,fd,m_pTCPKernel);
+				pool->append_task(task);
 			}
 			else
 			{
@@ -133,22 +152,21 @@ void TCPNet::DealData()
 	pool->stop();
 }
 
-bool TCPNet::SendData(int sockfd, char* szbuf, int nlen)
+bool TCPNet::SendData(int sockfd,char *szbuf,int nlen)
 {
-	if(!szbuf || nlen <= 0)
+	if(!szbuf||nlen<=0)
+	{
 		return false;
-	
-	//发送包的大小
-	if(write(sockfd,(const char*)&nlen,sizeof(int)) < 0)
+	}
+	//包大小
+	if(write(sockfd,(const char*)&nlen,sizeof(int))<0)
 		return false;
-	
-	//发送包的内容
-	if(write(sockfd, szbuf, nlen) < 0)
+	//包内容
+	if(write(sockfd,szbuf,nlen)<0)
 		return false;
 
-	return true;
+		return true;
 }
-
 void TCPNet::UnInitNetWork()
 {
 	close(sockfd);
